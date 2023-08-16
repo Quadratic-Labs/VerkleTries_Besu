c99f1c3477a95e3b36ebdf3b97bb9ba1d69401fe
Thomas Zamojski <thomas.zamojski@quadratic-labs.com>
2023-08-16 23:47:39 +0200

    ENH: simplified types of nodes

---
+++ ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/RemoveVisitor.java
@@ -0,0 +1,25 @@
+package org.hyperledger.besu.ethereum.trie.verkle;
+
+import org.apache.tuweni.bytes.Bytes;
+
+public class RemoveVisitor<V> implements PathNodeVisitor<V> {
+
+    @Override
+    public Node<V> visit(BranchNode<V> branchNode, Bytes path) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+    }
+
+    @Override
+    public Node<V> visit(LeafNode<V> leafNode, Bytes path) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+    }
+
+    @Override
+    public Node<V> visit(NullNode<V> nullNode, Bytes path) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+    }
+    
+}
