59fb7d8ace5846855ae5e441ca62de68679e4a52
Thomas Zamojski <thomas.zamojski@quadratic-labs.com>
2023-09-19 10:53:08 +0200

    ADD: TrieKeyAdaptor to compute trie keys
    VerkleTrie keys are created from addresses and indices.
    The transformation is referenced here: https://notes.ethereum.org/@vbuterin/verkle_tree_eip
    
    Note: Relies on PedersenHash, which in turn relies on NativeLib. Currently, commit in the nativelib is not fully implemented. Tests will need modification once this is done.

---
+++ ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/TrieKeyAdapter.java
@@ -0,0 +1,75 @@
+package org.hyperledger.besu.ethereum.trie.verkle;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import org.apache.tuweni.units.bigints.UInt256;
+import org.hyperledger.besu.nativelib.ipamultipoint.LibIpaMultipoint;
+
+
+public class TrieKeyAdapter {
+    private final UInt256 VERSION_LEAF_KEY = UInt256.valueOf(0);
+    private final UInt256 BALANCE_LEAF_KEY = UInt256.valueOf(1);
+    private final UInt256 NONCE_LEAF_KEY = UInt256.valueOf(2);
+    private final UInt256 CODE_KECCAK_LEAF_KEY = UInt256.valueOf(3);
+    private final UInt256 CODE_SIZE_LEAF_KEY = UInt256.valueOf(4);
+    private final UInt256 HEADER_STORAGE_OFFSET = UInt256.valueOf(64);
+    private final UInt256 CODE_OFFSET = UInt256.valueOf(128);
+    private final UInt256 VERKLE_NODE_WIDTH = UInt256.valueOf(256);
+    private final UInt256 MAIN_STORAGE_OFFSET = UInt256.valueOf(256).pow(31);
+
+    public TrieKeyAdapter() {}
+
+    Bytes32 swapLastByte(Bytes32 base, UInt256 subIndex) {
+        Bytes32 key = (Bytes32) Bytes.concatenate(base.slice(0, 31), Bytes.of(subIndex.toBytes().get(31)));
+        return key;
+    }
+
+    Bytes32 baseKey(Bytes32 address, UInt256 treeIndex) {
+        PedersenHash hasher = new PedersenHash();
+        Bytes32 hash = hasher.digest(Bytes.concatenate(address, treeIndex.toBytes().reverse()));
+        return hash;
+    }
+
+    public Bytes32 storageKey(Bytes32 address, UInt256 storageKey) {
+        UInt256 headerOffset = CODE_OFFSET.subtract(HEADER_STORAGE_OFFSET);
+        UInt256 offset = ((storageKey.compareTo(headerOffset) < 0) ? HEADER_STORAGE_OFFSET : MAIN_STORAGE_OFFSET);
+        UInt256 pos = offset.add(storageKey);
+        Bytes32 base = baseKey(address, pos.divide(VERKLE_NODE_WIDTH));
+        Bytes32 key = swapLastByte(base, pos.mod(VERKLE_NODE_WIDTH));
+        return key;
+    }
+
+    public Bytes32 codeChunkKey(Bytes32 address, UInt256 chunkId) {
+        UInt256 pos = CODE_OFFSET.add(chunkId);
+        Bytes32 base = baseKey(address, pos.divide(VERKLE_NODE_WIDTH));
+        Bytes32 key = swapLastByte(base, pos.mod(VERKLE_NODE_WIDTH));
+        return key;
+    }
+
+    // Headers
+    Bytes32 headerKey(Bytes32 address, UInt256 leafKey) {
+        Bytes32 base = baseKey(address, UInt256.valueOf(0));
+        Bytes32 key = swapLastByte(base, leafKey);
+        return key;
+    }
+
+    public Bytes32 versionKey(Bytes32 address) {
+        return headerKey(address, VERSION_LEAF_KEY);
+    }
+
+    public Bytes32 balanceKey(Bytes32 address) {
+        return headerKey(address, BALANCE_LEAF_KEY);
+    }
+
+    public Bytes32 nonceKey(Bytes32 address) {
+        return headerKey(address, NONCE_LEAF_KEY);
+    }
+
+    public Bytes32 codeKeccakKey(Bytes32 address) {
+        return headerKey(address, CODE_KECCAK_LEAF_KEY);
+    }
+
+    public Bytes32 codeSizeKey(Bytes32 address) {
+        return headerKey(address, CODE_SIZE_LEAF_KEY);
+    }
+}
