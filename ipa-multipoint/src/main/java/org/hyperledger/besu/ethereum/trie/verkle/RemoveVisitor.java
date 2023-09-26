8a11d4cc4e44a50e793b7e6963c013c210ec623c
Naman Garg <0708ng@gmail.com>
2023-09-27 02:10:50 +0530

    add RemoveVisitor and unit tests

--- ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/RemoveVisitor.java
+++ ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/RemoveVisitor.java
@@ -3,23 +3,33 @@ package org.hyperledger.besu.ethereum.trie.verkle;
 import org.apache.tuweni.bytes.Bytes;
 
 public class RemoveVisitor<V> implements PathNodeVisitor<V> {
-
+    private final Node<V> NULL_NODE_RESULT = NullNode.instance();
     @Override
     public Node<V> visit(BranchNode<V> branchNode, Bytes path) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        final Bytes leafPath = branchNode.getPath();
+        final Bytes commonPath = leafPath.commonPrefix(path);
+        if (commonPath.compareTo(leafPath) != 0) {
+            return branchNode;
+        }
+        final Bytes pathSuffix = path.slice(commonPath.size());
+        final byte childIndex = pathSuffix.get(0);
+        branchNode.replaceChild(childIndex, NULL_NODE_RESULT);
+        return branchNode;
     }
 
     @Override
     public Node<V> visit(LeafNode<V> leafNode, Bytes path) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        final Bytes nodePath = leafNode.getPath();
+        final Bytes commonPath = nodePath.commonPrefix(path);
+        if (commonPath.compareTo(nodePath) != 0) {
+            return leafNode;
+        }
+        return NULL_NODE_RESULT;
     }
 
     @Override
     public Node<V> visit(NullNode<V> nullNode, Bytes path) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        return NULL_NODE_RESULT;
     }
     
 }
--- ipa-multipoint/src/test/java/org/hyperledger/besu/ethereum/trie/verkle/SimpleVerkleTrieTest.java
+++ ipa-multipoint/src/test/java/org/hyperledger/besu/ethereum/trie/verkle/SimpleVerkleTrieTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
+import org.mockito.internal.matchers.InstanceOf;
 
 public class SimpleVerkleTrieTest {
     
@@ -106,5 +107,80 @@ public class SimpleVerkleTrieTest {
         assertThat(trie.get(key2)).as("Retrieve second value").isEqualTo(Optional.of(value2));
         Bytes32 expectedRootHash = Bytes32.fromHexString("0x93e0e732253644b0580af0a2d5d0d0bc82d1fb1a4fb9f0b49b65b5db9020b025");
         assertThat(trie.getRootHash()).as("Retrieve root hash").isEqualByComparingTo(expectedRootHash);
+    }
+
+    @Test
+    public void testDeleteTwoValuesAtSameStem() throws Exception {
+        SimpleVerkleTrie<Bytes32, Bytes32> trie = new SimpleVerkleTrie<Bytes32, Bytes32>();
+        Bytes32 key1 = Bytes32.fromHexString("0x00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff");
+        Bytes32 value1 = Bytes32.fromHexString("0x1000000000000000000000000000000000000000000000000000000000000001");
+        Bytes32 key2 = Bytes32.fromHexString("0x00112233445566778899aabbccddeeff00112233445566778899aabbccddee00");
+        Bytes32 value2 = Bytes32.fromHexString("0x0100000000000000000000000000000000000000000000000000000000000002");
+        Bytes path = Bytes.fromHexString("0x00112233445566778899aabbccddeeff00112233445566778899aabbccddee");
+        byte index1 = key1.get(31);
+        byte index2 = key2.get(31);
+        byte index3 = (byte) 2;
+        trie.put(key1, value1);
+        trie.put(key2, value2);
+        Node<Bytes32> root = trie.getRoot();
+        assertThat(root).as("Many values trie is a BranchNode").isInstanceOf(BranchNode.class);
+        BranchNode<Bytes32> branchRoot = (BranchNode<Bytes32>) root;
+
+        trie.remove(key1);
+        assertThat(trie.get(key1)).as("Make sure value is deleted").isEqualTo(Optional.empty());
+        assertThat(branchRoot.child(index1)).as("Child at index of first key is a NullNode").isInstanceOf(NullNode.class);
+        trie.remove(key2);
+        assertThat(trie.get(key2)).as("Make sure value is deleted").isEqualTo(Optional.empty());
+        assertThat(branchRoot.child(index2)).as("Child at index of second key is a NullNode").isInstanceOf(NullNode.class);
+    }
+
+    @Test
+    public void testDeleteTwoValuesAtDifferentIndex() throws Exception {
+        SimpleVerkleTrie<Bytes32, Bytes32> trie = new SimpleVerkleTrie<Bytes32, Bytes32>();
+        Bytes32 key1 = Bytes32.fromHexString("0x00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff");
+        Bytes32 value1 = Bytes32.fromHexString("0x1000000000000000000000000000000000000000000000000000000000000000");
+        Bytes32 key2 = Bytes32.fromHexString("0xff112233445566778899aabbccddeeff00112233445566778899aabbccddee00");
+        Bytes32 value2 = Bytes32.fromHexString("0x0100000000000000000000000000000000000000000000000000000000000000");
+        Bytes path = Bytes.fromHexString("0x");
+        byte index1 = key1.get(0);
+        byte index2 = key2.get(0);
+        byte index3 = (byte) 2;
+        trie.put(key1, value1);
+        trie.put(key2, value2);
+        Node<Bytes32> root = trie.getRoot();
+        assertThat(root).as("Many values trie's root is a BranchNode").isInstanceOf(BranchNode.class);
+        BranchNode<Bytes32> branchRoot = (BranchNode<Bytes32>) root;
+
+        trie.remove(key1);
+        assertThat(trie.get(key1)).as("Make sure value is deleted").isEqualTo(Optional.empty());
+        assertThat(branchRoot.child(index1)).as("Child at index of first key is a NullNode").isInstanceOf(NullNode.class);
+        trie.remove(key2);
+        assertThat(trie.get(key2)).as("Make sure value is deleted").isEqualTo(Optional.empty());
+        assertThat(branchRoot.child(index2)).as("Child at index of second key is a NullNode").isInstanceOf(NullNode.class);
+    }
+
+    @Test
+    public void testDeleteTwoValuesWithDivergentStemsAtDepth2() throws Exception {
+        SimpleVerkleTrie<Bytes32, Bytes32> trie = new SimpleVerkleTrie<Bytes32, Bytes32>();
+        Bytes32 key1 = Bytes32.fromHexString("0x00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff");
+        Bytes32 value1 = Bytes32.fromHexString("0x1000000000000000000000000000000000000000000000000000000000000000");
+        Bytes32 key2 = Bytes32.fromHexString("0x00ff112233445566778899aabbccddeeff00112233445566778899aabbccddee");
+        Bytes32 value2 = Bytes32.fromHexString("0x0100000000000000000000000000000000000000000000000000000000000000");
+        Bytes path = Bytes.fromHexString("0x00");
+        byte index1 = key1.get(1);
+        byte index2 = key2.get(1);
+        byte index3 = (byte) 0;
+        trie.put(key1, value1);
+        trie.put(key2, value2);
+        Node<Bytes32> root = trie.getRoot();
+        assertThat(root).as("Many values trie's root is a BranchNode").isInstanceOf(BranchNode.class);
+        BranchNode<Bytes32> branchRoot = (BranchNode<Bytes32>) root;
+
+        trie.remove(key1);
+        assertThat(trie.get(key1)).as("Make sure value is deleted").isEqualTo(Optional.empty());
+        assertThat(branchRoot.child(index1)).as("Child at index of first key is a NullNode").isInstanceOf(NullNode.class);
+        trie.remove(key2);
+        assertThat(trie.get(key2)).as("Make sure value is deleted").isEqualTo(Optional.empty());
+        assertThat(branchRoot.child(index2)).as("Child at index of second key is a NullNode").isInstanceOf(NullNode.class);
     }
 }
