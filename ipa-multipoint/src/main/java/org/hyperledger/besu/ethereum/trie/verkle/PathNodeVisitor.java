025df3f23f757076002487156bb86a1fd37b09bd
Thomas Zamojski <thomas.zamojski@quadratic-labs.com>
2023-09-28 02:27:19 +0200

    FIX: signing the commit

--- ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/PathNodeVisitor.java
+++ ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/PathNodeVisitor.java
@@ -2,7 +2,13 @@ package org.hyperledger.besu.ethereum.trie.verkle;
 
 import org.apache.tuweni.bytes.Bytes;
 
-
+/**
+ * Visit the trie with a path parameter.
+ * 
+ * The path parameter indicates the path visited in the Trie.
+ * As Nodes are (mostly) immutable, visiting a Node returns a (possibly) new Node that should replace the old one.
+ * 
+ */
 public interface PathNodeVisitor<V> {
 
     Node<V> visit(BranchNode<V> branchNode, Bytes path);
