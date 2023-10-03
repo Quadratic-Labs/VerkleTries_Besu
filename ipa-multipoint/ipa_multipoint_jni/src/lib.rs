0e2b871c046fec9e6570d4eb4b3c5b6829680a76
Dragan Pilipovic <draganpilipovic1997bp@gmail.com>
2023-10-03 05:51:17 +0200

    revert work on pedersen hash

--- ipa-multipoint/ipa_multipoint_jni/src/lib.rs
+++ ipa-multipoint/ipa_multipoint_jni/src/lib.rs
@@ -56,3 +56,116 @@ pub extern "system" fn Java_org_hyperledger_besu_nativelib_ipamultipoint_LibIpaM
     commit.reverse();
     return env.byte_array_from_slice(&commit).expect("Couldn't convert to byte array")
 }
+
+#[no_mangle]
+pub extern "system" fn Java_org_hyperledger_besu_nativelib_ipamultipoint_LibIpaMultipoint_pedersenHash(
+    env: JNIEnv,
+    _class: JClass,
+    input: jbyteArray,
+) -> jbyteArray {
+    // First, we have to get the byte[] out of java.
+    let ascii_bytes = env.convert_byte_array(input).unwrap();
+    let mut helper_array: [u8; 128] = [0; 128];
+
+    helper_array.copy_from_slice(&ascii_bytes);
+
+
+    let mut merged_values: Vec<u8> = Vec::new();
+    // Iterate over pairs of elements and merge them into u8 values so we can use them for the pedersen hash.
+    for chunk in helper_array.chunks(2) {
+        if chunk.len() == 2 {
+            let merged_byte = (chunk[0] << 4) | (chunk[1] & 0x0F);
+            merged_values.push(merged_byte);
+        }
+    }
+
+    // Now we create the address and trie index variables.
+    let mut address32 = [0u8; 32];
+    address32.copy_from_slice(&merged_values[0..32]);
+    let mut trie_index= [0u8; 32];
+    trie_index.copy_from_slice(&merged_values[32..64]);
+
+    // Now we compute the pedersen hash
+    let mut result = pedersen_hash(&address32.to_vec(), &trie_index.to_vec());
+
+
+    // TODO: make this prettier, this is very ugly.
+    let mut build_string = String::from("");
+    for i in 0..32 {
+        let hex_string = format!("{:02x}", &result[i]);
+        build_string = format!("{}{}", &build_string, hex_string.clone());
+    }
+    // TODO: remove next 3 lines, it's for testing purposes.
+    // let mut str1 = "946352acd92aba2884d2b8746f44ae5fa1f61cc424af5c1a74c5c688862e2e48";
+    // let mut str2 = re.as_str();
+    // assert_eq!(str1, re.as_str());
+    let result_array = turn_str_to_bytes(build_string.as_str());
+    let output = env.byte_array_from_slice(&result_array).unwrap();
+    output
+}
+
+/// Pedersen hash receives an address and a trie index and returns a hash calculated this way:
+/// H(constant || address_low || address_high || trie_index_low || trie_index_high)
+/// where constant = 2 + 256*64
+/// address_low = first 16 bytes of the address
+/// address_high = last 16 bytes of the address
+/// trie_index_low = first 16 bytes of the trie index
+/// trie_index_high = last 16 bytes of the trie index
+/// The result is a 256 bit hash
+/// TODO: make the parameters prettier
+fn pedersen_hash(address_input : &Vec<u8>, trie_index_input : &Vec<u8>) -> [u8; 32] {
+    let constant = Fr::from(2u128 + 256u128*64u128); // Hardcoded constant
+
+    let address_reference_to_vec: &Vec<u8> = &address_input;
+    let address: &[u8] = &*address_reference_to_vec;
+
+    let trie_index_reference_to_vec: &Vec<u8> = &trie_index_input;
+    let trie_index: &[u8] = &*trie_index_reference_to_vec;
+
+    // TODO: remove next 2 lines, it's used for testing.
+    // let address = "200000000000000000000000b794f5ea0ba39494ce839613fffba74279579268".as_bytes();
+    // let trie_index = "2000000000000000000000000000000000000000000000000000000000000001".as_bytes();
+
+    let address_low = Fr::from_le_bytes_mod_order(&address[0..16]);
+
+    let address_high = Fr::from_le_bytes_mod_order(&address[16..32]);
+
+    let trie_index_low = Fr::from_le_bytes_mod_order(&trie_index[0..16]);
+
+    let trie_index_high = Fr::from_le_bytes_mod_order(&trie_index[16..32]);
+
+    // Create a vector with the scalars. The first one is the constant, the rest are the values of the address and trie index
+    let scalars = vec![constant, address_low.clone(), address_high.clone(), trie_index_low.clone(), trie_index_high.clone()];
+
+    // Generate the CRS
+    let bases = CRS::new(5, "eth_verkle_oct_2021".as_bytes());
+
+    // Compute the multi scalar multiplication. The result is a point in the banderwagon group.
+    let mut result = multi_scalar_mul(&bases.G, &scalars).to_bytes();
+
+    // Reverse the result to get the correct order of the bytes.
+    result.reverse();
+    return result;
+}
+
+/// Helper function for Java JNI.
+/// It receives a hexadecimal string and returns a [u8; 64] array
+/// where each byte is the hexadecimal value of the letter in the string.
+/// This is needed because of the Rust and Java interoperability.
+fn turn_str_to_bytes(hex_string: &str) -> [u8; 64] {
+    // Create a vector to store the u8 integers
+    let mut u8_integers: Vec<u8> = Vec::new();
+
+    // Iterate over each character in the hexadecimal string
+    for hex_char in hex_string.chars() {
+        let hex_val = hex_char as u8;
+        u8_integers.push(hex_val);
+    }
+
+    let mut u8_array = [0u8; 64];
+
+    // Convert the vector into an array of u8 integers
+    u8_array.copy_from_slice(&u8_integers);
+
+    u8_array
+}
--- ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/LibIpaMultipointTest.java
+++ ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/LibIpaMultipointTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.nativelib.ipamultipoint.LibIpaMultipoint;
+import java.util.Arrays;
 
 
 public class LibIpaMultipointTest {
@@ -57,5 +58,22 @@ public class LibIpaMultipointTest {
         Bytes32 result = Bytes32.wrap(LibIpaMultipoint.commit(input.toArray()));
         Bytes32 expected = Bytes32.fromHexString("0x901f1fb216d7903987a1f021284dfdec5dc39c53617aacd4640bb8e81758ab06");
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void testCallLibraryPedersenHash() {
+        // I've put 2 in front for testing reason, real values will rarely start with 2.
+        Bytes32 address = Bytes32.fromHexString("0x200000000000000000000000b794f5ea0ba39494ce839613fffba74279579268");
+        Bytes32 trieIndex = Bytes32.fromHexString("0x2000000000000000000000000000000000000000000000000000000000000001");
+        Bytes total = Bytes.wrap(address,trieIndex);
+        String totalString = total.toHexString().substring(2); // subtract leading "0x"
+        byte[] totalStringBytes = totalString.getBytes();
+        byte[] result = LibIpaMultipoint.pedersenHash(totalStringBytes);
+        // expected value
+        Bytes total2 = Bytes32.fromHexString("0xbc27c15f46d538933a54f7cb793ab2310ee0f50996e3bac22c0acdc05715e95c");
+        String totalString2 = total2.toHexString().substring(2); // subtract leading "0x"
+        byte[] totalStringBytes2 = totalString2.getBytes();
+        assertThat(result).isEqualTo(totalStringBytes2);
+        System.out.println(Arrays.toString(result));
     }
 }
