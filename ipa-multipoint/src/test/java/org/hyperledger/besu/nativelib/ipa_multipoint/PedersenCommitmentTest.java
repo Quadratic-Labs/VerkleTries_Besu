3cc228e36b18f2d83cfe9ebaae0cb27c2a1cbcf7
Thomas Zamojski <thomas.zamojski@quadratic-labs.com>
2023-10-20 05:52:19 +0200

    FIX: remove unnecessary artifacts from testing

--- ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/PedersenCommitmentTest.java
+++ ipa-multipoint/src/test/java/org/hyperledger/besu/nativelib/ipa_multipoint/PedersenCommitmentTest.java
@@ -1,6 +1,5 @@
 package org.hyperledger.besu.nativelib.ipa_multipoint;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.assertj.core.api.Assertions.*;
@@ -23,7 +22,6 @@ public class PedersenCommitmentTest {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static List<TestData> JsonData() throws IOException {
-        System.out.println(PedersenCommitmentTest.class.getResource("/").getPath());
         InputStream inputStream = PedersenCommitmentTest.class.getResourceAsStream("/pedersen_commitment_test.json");
         return objectMapper.readValue(inputStream, new TypeReference<List<TestData>>() {});
     }
@@ -45,19 +43,5 @@ public class PedersenCommitmentTest {
         BigInteger result = Bytes32.wrap(LibIpaMultipoint.commit(input)).toBigInteger();
         BigInteger expected = new BigInteger(testData.commitment);
         assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void TestGenerators() {
-        List<Bytes> FrBytes = new ArrayList<>();
-        for (int i = 0; i < 256; i++ ) {
-            FrBytes.add(i, Bytes32.ZERO);
-        }
-        for (int i = 0; i < 256; i++ ) {
-            FrBytes.set(i, Bytes32.leftPad(Bytes.of(1)));
-            byte[] input = Bytes.concatenate(FrBytes).toArray();
-            Bytes32 result = Bytes32.wrap(LibIpaMultipoint.commit(input));
-            System.out.println(String.format("%s", result.toHexString()));
-        }
     }
 }
