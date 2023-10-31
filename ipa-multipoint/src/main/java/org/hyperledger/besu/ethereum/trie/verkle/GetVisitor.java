c6050c2d2eb998a579b5b9cfd5cd02d9a8bfa0a2
Filip.Gumula <filgum0326@gmail.com>
2023-10-31 15:08:08 +0100

    Added javadoc comments to GetVisitor

--- ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/GetVisitor.java
+++ ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/GetVisitor.java
@@ -2,9 +2,21 @@ package org.hyperledger.besu.ethereum.trie.verkle;
 
 import org.apache.tuweni.bytes.Bytes;
 
+/**
+ * Class representing a visitor for traversing nodes in a Trie tree to find a node based on a path.
+ *
+ * @param <V> The type of node values.
+ */
 public class GetVisitor<V> implements PathNodeVisitor<V> {
     private final Node<V> NULL_NODE_RESULT = NullNode.instance();
 
+    /**
+     * Visits a BranchNode to determine the node matching a given path.
+     *
+     * @param branchNode The BranchNode being visited.
+     * @param path The path to search in the tree.
+     * @return The matching node or NULL_NODE_RESULT if not found.
+     */
     @Override
     public Node<V> visit(final BranchNode<V> branchNode, final Bytes path) {
         final Bytes nodePath = branchNode.getPath();
@@ -18,6 +30,13 @@ public class GetVisitor<V> implements PathNodeVisitor<V> {
         return branchNode.child(childIndex).accept(this, pathSuffix.slice(1));
     }
 
+    /**
+     * Visits a LeafNode to determine the matching node based on a given path.
+     *
+     * @param leafNode The LeafNode being visited.
+     * @param path The path to search in the tree.
+     * @return The matching node or NULL_NODE_RESULT if not found.
+     */
     @Override
     public Node<V> visit(LeafNode<V> leafNode, Bytes path) {
         final Bytes leafPath = leafNode.getPath();
@@ -28,6 +47,13 @@ public class GetVisitor<V> implements PathNodeVisitor<V> {
         return leafNode;
     }
 
+    /**
+     * Visits a NullNode to determine the matching node based on a given path.
+     *
+     * @param nullNode The NullNode being visited.
+     * @param path The path to search in the tree.
+     * @return The NULL_NODE_RESULT since NullNode represents a missing node on the path.
+     */
     @Override
     public Node<V> visit(NullNode<V> nullNode, Bytes path) {
         return NULL_NODE_RESULT;
