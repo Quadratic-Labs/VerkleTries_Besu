c3b1ca22fe8b52107c393085eeab2974105b2cae
Filip.Gumula <filgum0326@gmail.com>
2023-10-31 16:03:23 +0100

    Added javadoc comments for SHA256Hasher

--- ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/SHA256Hasher.java
+++ ipa-multipoint/src/main/java/org/hyperledger/besu/ethereum/trie/verkle/SHA256Hasher.java
@@ -6,8 +6,20 @@ import java.security.NoSuchAlgorithmException;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 
-
+/**
+ * A class responsible for hashing an array of Bytes32 using the SHA-256 (Secure Hash Algorithm 256-bit) hashing algorithm.
+ *
+ * <p>This class implements the Hasher interface and provides a method to commit multiple Bytes32 inputs using the SHA-256 hashing algorithm.
+ * It utilizes the Java built-in MessageDigest for SHA-256.
+ */
 public class SHA256Hasher implements Hasher<Bytes32> {
+
+    /**
+     * Commits an array of Bytes32 using the SHA-256 hashing algorithm provided by the MessageDigest.
+     *
+     * @param inputs An array of Bytes32 inputs to be hashed and committed.
+     * @return The resulting hash as a Bytes32.
+     */
     @Override
     public Bytes32 commit(Bytes32[] inputs) {
         Bytes32 out;
